116bbc2a4e4560955c1b2ba56c5537f18592d7d3
asa151930 <68521426+asa151930@users.noreply.github.com>
2021-03-24 19:28:32 -0500

    conflict resolved

--- watchlist.js
+++ watchlist.js
@@ -1,53 +1,4 @@
-<<<<<<< HEAD
-$(document).ready(function () {
-  $(".sidenav").sidenav();
-  $('.tap-target').tapTarget();
 
-
-  let storedTitles = "";
-  function displayBookmarks() {
-
-    storedTitles = JSON.parse(localStorage.getItem("showTitle"));
-
-
-    for (let i = 0; i < storedTitles.length; i++) {
-      let titleLi = $("<li class='collection-item'>");
-      titleLi.text(storedTitles[i]);
-      $("#stored-titles").append(titleLi);
-    }
-    if (storedTitles !== "") {
-      $("#stored-titles").attr("class", "collection")
-    }
-  }
-
-
-  function chuckNorris() {
-    fetch("https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random", {
-      "method": "GET",
-      "headers": {
-        "accept": "application/json",
-        "x-rapidapi-key": "07375822b7mshfbcf7e4725a7defp1bf4cbjsnd4cef9f733f3",
-        "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com"
-      }
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((response) => {
-        console.log(response);
-        $("#chuck-norris").text(response.value);
-
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
-
-  chuckNorris();
-
-  displayBookmarks();
-});
-=======
 // opens ham
 $(".sidenav").sidenav();
 $('.tap-target').tapTarget();
@@ -88,4 +39,4 @@ displayBookmarks();
 
 // calling display jokes
 jokeDisplay();
->>>>>>> 1a98e68e0db2258a9c2573c53e6693b37d5154c0
+
